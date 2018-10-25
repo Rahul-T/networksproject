d25186b96e54e6ec3aed90f868b1241a39b6149a
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2018-10-24 23:33:13 -0400

    Peer discovery can now send dict as payload and add incoming new nodes

--- star-node.py
+++ star-node.py
@@ -1,20 +1,28 @@
 from socket import *
 import sys
+import json
 
-def peerdiscovery(name, pocAddress, pocPort):
-	pdpacket = "000"
-	pdpacket = pdpacket + name
+def peerdiscovery(name, pocAddress, pocPort, knownNodes):
+	pdpacket = "000" + "{:<16}".format(name) + json.dumps(knownNodes)
 	print(pdpacket)
 	serverSocket.sendto(pdpacket.encode(), (pocAddress, int(pocPort)))
 	incomingPacket, address = serverSocket.recvfrom(2048)
 	output = incomingPacket.decode()
-	print(output)
+	incKnownNodes = json.loads(output[19:])
+	for node in incKnownNodes:
+		if node not in knownNodes:
+			knownNodes[node] = incKnownNodes[node]
+	print(knownNodes)
 
 name = sys.argv[1]
 localPort = sys.argv[2]
 pocAddress = sys.argv[3]
 pocPort = sys.argv[4]
 maxNodes = sys.argv[5]
+
+knownNodes = {name:True}
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', int(localPort)))
-peerdiscovery(name, pocAddress, pocPort)
+
+peerdiscovery(name, pocAddress, pocPort, knownNodes)
