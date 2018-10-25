c96be66d65ed6e8a67c2836a29d46634e77f3213
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2018-10-25 00:27:07 -0400

    Differentiated initial poc and peer discovery methods, runs peer discovery on newly discovered nodes

--- star-node.py
+++ star-node.py
@@ -1,11 +1,25 @@
-from socket import *
 import sys
 import json
+import socket
 
-def peerdiscovery(name, pocAddress, pocPort, knownNodes):
+def initialPoc(name, pocAddress, pocPort, knownNodes):
 	pdpacket = "000" + "{:<16}".format(name) + json.dumps(knownNodes)
-	print(pdpacket)
+	#print(pdpacket)
 	serverSocket.sendto(pdpacket.encode(), (pocAddress, int(pocPort)))
+	incomingPacket, address = serverSocket.recvfrom(2048)
+	output = incomingPacket.decode()
+	incKnownNodes = json.loads(output[19:])
+	for node in incKnownNodes:
+		if node not in knownNodes:
+			knownNodes[node] = incKnownNodes[node]
+	#print(knownNodes)
+
+def peerDiscovery(name, knownNodes):
+	pdpacket = "000" + "{:<16}".format(name) + json.dumps(knownNodes)
+	#print(pdpacket)
+	for node in knownNodes:
+		if node != name:
+			serverSocket.sendto(pdpacket.encode(), (knownNodes[node][0], int(knownNodes[node][1])))
 	incomingPacket, address = serverSocket.recvfrom(2048)
 	output = incomingPacket.decode()
 	incKnownNodes = json.loads(output[19:])
@@ -20,9 +34,11 @@ pocAddress = sys.argv[3]
 pocPort = sys.argv[4]
 maxNodes = sys.argv[5]
 
-knownNodes = {name:True}
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+knownNodes = {name:[socket.gethostname(), localPort]}
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(('', int(localPort)))
 
-peerdiscovery(name, pocAddress, pocPort, knownNodes)
+initialPoc(name, pocAddress, pocPort, knownNodes)
+peerDiscovery(name, knownNodes)
