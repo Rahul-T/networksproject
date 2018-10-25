b8b758b4f4fe2c151e582a9bea751dc265e4180c
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2018-10-24 22:23:04 -0400

    Work-in-progress peer discovery method

--- star-node.py
+++ star-node.py
@@ -1,13 +1,20 @@
 from socket import *
 import sys
 
+def peerdiscovery(name, pocAddress, pocPort):
+	pdpacket = "000"
+	pdpacket = pdpacket + name
+	print(pdpacket)
+	serverSocket.sendto(pdpacket.encode(), (pocAddress, int(pocPort)))
+	incomingPacket, address = serverSocket.recvfrom(2048)
+	output = incomingPacket.decode()
+	print(output)
+
 name = sys.argv[1]
-localport = sys.argv[2]
-pocaddress = sys.argv[3]
-pocport = sys.argv[3]
-maxnodes = sys.argv[4]
-
+localPort = sys.argv[2]
+pocAddress = sys.argv[3]
+pocPort = sys.argv[4]
+maxNodes = sys.argv[5]
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', localPort))
-
-def peerdiscovery():
+serverSocket.bind(('', int(localPort)))
+peerdiscovery(name, pocAddress, pocPort)
