af6c72ef2a1cd5881eafc09c45e103cd7ba1b14b
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2018-11-24 00:29:47 -0700

    Added packet loss/duplication functionality, currently slow, will fix by multithreading

--- star-node.py
+++ star-node.py
@@ -35,8 +35,8 @@ class Peer:
 		self.packetNum = 0
 		self.packetTimes = {}
 
-		self.receivedPackets = {}
-		self.receivedAcks = {}
+		self.receivedPackets = set()
+		self.receivedAcks = set()
 
 	    # used to stop the main loop
 		self.shutdown = False  
@@ -87,9 +87,13 @@ class Peer:
 		t5 = threading.Thread( target = self.__handlepeer, args = [ s ] )
 		t5.daemon = True
 		t5.start()
+		t7 = threading.Thread( target = self.__handlepeer, args = [ s ] )
+		t7.daemon = True
+		t7.start()
 		t6 = threading.Thread(target = self.updaterttsandhub, args = [ s ])
 		t6.daemon = True
 		t6.start()
+
 		while not self.shutdown:
 			try:
 				if not t4.isAlive() and not self.shutdown:
@@ -99,6 +103,10 @@ class Peer:
 					t5 = threading.Thread( target = self.__handlepeer, args = [ s ] )
 					t5.daemon = True
 					t5.start()
+				if not t7.isAlive():
+					t7 = threading.Thread( target = self.__handlepeer, args = [ s ] )
+					t7.daemon = True
+					t7.start()
 				if not t6.isAlive():
 					t6 = threading.Thread(target = self.updaterttsandhub, args = [ s ])
 					t6.daemon = True
@@ -138,26 +146,44 @@ class Peer:
 		output = message.decode()
 		port = output[3:8]
 		try:
-
+			#print(output)
 			msgtype = output[0:3]
 			if msgtype == "000":
-				self.receivePeerDiscovery(clientAddress, output)
+				#print("Received peer discovery from " + output[8:24].strip())
+				self.receivePeerDiscovery(clientAddress, output, socket)
 			if msgtype == "001":
 				nodeName = output[8:24]
+				#print("received rtt pack from " + nodeName)
 				packetNumber = output[24:]
 				self.initialRTTack(socket, nodeName.strip(), packetNumber)
 			if msgtype == "002":
 				nodeName = output[8:24].strip()
+				#print("got ack from " + nodeName)
 				endTime = time.time()
+				#print(self.packetTimes)
+				#print("packettimes index " + str(output[24:].strip()))
+				#print(self.packetTimes)
+				#print("pack above")
 				rtttime = endTime - self.packetTimes[int(output[24:].strip())]
 				self.rtttimes[nodeName] = rtttime
+				confpacknum = int(output[24:].strip())
+				#print("conf")
+				#print(confpacknum)
+				self.receivedAcks.add(confpacknum)
 			if msgtype == "003":
-				#print("received rttsum packet from " + output[8:24].strip())
-				self.receiveRTTsum(clientAddress, output)
+				self.receiveRTTsum(clientAddress, output, socket)
 			if msgtype == "004":
+				#print("got string")
 				self.receiveStringMessage(clientAddress, output, socket)
 			if msgtype == "005":
 				self.receiveFile(clientAddress, output, socket, message)
+			if msgtype == "006":
+				#print("Received ack")
+				confpacknum = int(message[24:].strip())
+				self.receivedAcks.add(confpacknum)
+				#print("Received ack from " + output[8:24].strip())
+				#print(self.packetNum)
+				#print(self.receivedAcks)
 
 		except KeyboardInterrupt:
 			raise
@@ -188,29 +214,54 @@ class Peer:
 	#Peer Discovery
 
 	def initialPoc(self, serverSocket):
-		pdpacket = "000" + "{:<5}".format(localPort) + "{:<16}".format(name) + json.dumps(self.peers)
-		serverSocket.sendto(pdpacket.encode(), (pocAddress, int(pocPort)))
+		temppacketnum = int(self.packetNum)
+		self.packetNum += 1
+		while int(temppacketnum) not in self.receivedAcks:
+			#print("Sending packet " + str(temppacketnum) + " to poc")
+			#print(self.receivedAcks)
+			pdpacket = "000" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<100}".format(temppacketnum) + json.dumps(self.peers)
+			serverSocket.sendto(pdpacket.encode(), (pocAddress, int(pocPort)))
+			#print("sleep1")
+			time.sleep(1)
+		#print("the end")
 
 	def sendPeerDiscovery(self, serverSocket):
-		pdpacket = "000" + "{:<5}".format(localPort) + "{:<16}".format(name) + json.dumps(self.peers)
+		temppacketnum = int(self.packetNum)
+		self.packetNum += 1
 		for node in self.peers:
 			if node != name:
-				serverSocket.sendto(pdpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+				while int(temppacketnum) not in self.receivedAcks:
+					#print("Sending packet " + str(temppacketnum) + " to node " + str(node))
+					pdpacket = "000" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<100}".format(temppacketnum) + json.dumps(self.peers)
+					serverSocket.sendto(pdpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+					#print("sleep2")
+					time.sleep(1)
+			temppacketnum = int(self.packetNum)
+			self.packetNum += 1
 
-	def receivePeerDiscovery(self, clientAddress, message):
-		incKnownNodes = json.loads(message[23:])
-		for node in incKnownNodes:
-			if node not in self.peers:
-				self.log = open(self.logfilename, 'a')
-				self.log.write("First discovered node " + str(node) + " at " + str(time.time()) + "\n")
-				self.log.close()
-				self.peers[node] = incKnownNodes[node]
+	def receivePeerDiscovery(self, clientAddress, message, serverSocket):
+		packnum = int(message[24:123].strip())
+		#print("Node name is " + message[8:24].strip())
+		if (message[8:24].strip(), packnum) not in self.receivedPackets:
+			self.receivedPackets.add((message[8:24].strip(), packnum))
+			incKnownNodes = json.loads(message[123:])
+			for node in incKnownNodes:
+				if node not in self.peers:
+					self.log = open(self.logfilename, 'a')
+					self.log.write("First discovered node " + str(node) + " at " + str(time.time()) + "\n")
+					self.log.close()
+					self.peers[node] = incKnownNodes[node]
+		ackpacket = "006" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<100}".format(packnum)
+		nodeName = message[8:24].strip()
+		serverSocket.sendto(ackpacket.encode(), (self.peers[nodeName][0], int(self.peers[nodeName][1])))
 
 	def initialPeerDiscovery(self, s):
 		timeoutcounter = 0
 		while len(self.peers) < int(maxNodes) and not self.shutdown:
 			try:
-				if pocAddress != 0 and pocPort != 0 and not [pocAddress, pocPort] in self.peers.values():
+				if str(pocAddress) != "0" and str(pocPort) != "0" and not [pocAddress, pocPort] in self.peers.values():
+					#print("NO ABC")
+					#print(str(pocAddress) + " " + str(pocPort) + " ")
 					self.initialPoc(s)
 				self.sendPeerDiscovery(s)
 			except KeyboardInterrupt:
@@ -223,110 +274,134 @@ class Peer:
 			timeoutcounter += 1
 			if timeoutcounter == 30:
 				self.shutdown = True
+			#print("sleep3")
 			time.sleep(1)
 		self.sendPeerDiscovery(s)
+		#print("finish")
 
 
 	#RTT
 
-	def initialSendRTT(self, serverSocket, node, packetNum):
-		initialrttpacket = "001" + "{:<5}".format(localPort) + "{:<16}".format(name) + str(packetNum)
-		self.log = open(self.logfilename, 'a')
-		self.log.write("Sent RTT request to " + str(node) + " at " + str(time.time()) + "\n")
-		self.log.close()
-		serverSocket.sendto(initialrttpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+	def initialSendRTT(self, serverSocket, node):
+		temppacketnum = int(self.packetNum)
+		self.packetNum += 1
+		while int(temppacketnum) not in self.receivedAcks:
+			self.startingTime = time.time()
+			self.packetTimes[int(temppacketnum)] = self.startingTime
+			#print(node)
+			#print("stuck")
+			initialrttpacket = "001" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<100}".format(temppacketnum)
+			self.log = open(self.logfilename, 'a')
+			self.log.write("Sent RTT request to " + str(node) + " at " + str(time.time()) + "\n")
+			self.log.close()
+			serverSocket.sendto(initialrttpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+			#print(temppacketnum)
+			#print(self.receivedAcks)
 
 
 	def initialRTTack(self, serverSocket, node, packetNumber):
-		initialrttpacket = "002" + "{:<5}".format(localPort) + "{:<16}".format(name) + packetNumber
+		if (node, int(packetNumber.strip())) not in self.receivedPackets:
+			self.receivedPackets.add((node, int(packetNumber.strip())))
+		#print("sent ack")
+		initialrttpacket = "002" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<100}".format(packetNumber)
 		self.log = open(self.logfilename, 'a')
 		self.log.write("Received RTT request from " + str(node) + " at " + str(time.time()) + "\n")
 		self.log.close()
 		serverSocket.sendto(initialrttpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
 
 	def sendRTTsum(self, serverSocket):
-		rttpacket = "003" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<16}".format(self.rttsum)
+		temppacketnum = int(self.packetNum)
+		self.packetNum += 1
 		#print(self.peers)
 		for node in self.peers:
 			if node != name:
-				#print("Sent my rttsum of " + str(self.rttsum) + " to " + node)
-				serverSocket.sendto(rttpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+				while int(temppacketnum) not in self.receivedAcks:
+					rttpacket = "003" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<32}".format(str(self.rttsum)) + "{:<100}".format(temppacketnum)
+					serverSocket.sendto(rttpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+					time.sleep(1)
+			temppacketnum = int(self.packetNum)
+			self.packetNum += 1
 
-	def receiveRTTsum(self, clientAddress, message):
-		
-		incRTTsum = message[23:]
+	def receiveRTTsum(self, clientAddress, message, serverSocket):
+		packnum = int(message[55:].strip())
 		nodeName = message[8:24].strip()
-		#print("Received rttsum of " + incRTTsum + " from " + nodeName)
-		self.log = open(self.logfilename, 'a')
-		self.log.write("Received new RTT sum from " + str(nodeName) + " at " + str(time.time()) + "\n")
-		self.log.close()
-		#print("stuck1")
-		if nodeName in self.rttsums:
-			oldRTTsum = self.rttsums[nodeName]
-			self.rttsums[nodeName] = incRTTsum
-		#print("stuck2")
-		if self.hubnode == name:
-			#print("stuck3")
-			if float(incRTTsum) + 0.25 < float(self.rttsum):
-				self.hubnode = nodeName
+		#print(message)
+		#print(packnum)
+		if (message[8:24].strip(), packnum) not in self.receivedPackets:
+			self.receivedPackets.add((message[8:24].strip(), packnum))
+			incRTTsum = message[23:55]
+			#print(incRTTsum)
+			#print("Received rttsum of " + incRTTsum + " from " + nodeName)
+			self.log = open(self.logfilename, 'a')
+			self.log.write("Received new RTT sum from " + str(nodeName) + " at " + str(time.time()) + "\n")
+			self.log.close()
+			#print("stuck1")
+			if nodeName in self.rttsums:
+				oldRTTsum = self.rttsums[nodeName]
+				self.rttsums[nodeName] = incRTTsum
+			#print("stuck2")
+			if self.hubnode == name:
+				#print("stuck3")
+				if float(incRTTsum) + 0.25 < float(self.rttsum):
+					self.hubnode = nodeName
+					self.log = open(self.logfilename, 'a')
+					self.log.write("Updated hub from " + self.hubnode + " to " + nodeName + " at " + str(time.time()) + "\n")
+					self.log.close()
+					#print("h1")
+			elif float(incRTTsum) + 0.25 < float(self.rttsums[self.hubnode]) and self.hubnode != nodeName:
+				#print("stuck4")
 				self.log = open(self.logfilename, 'a')
 				self.log.write("Updated hub from " + self.hubnode + " to " + nodeName + " at " + str(time.time()) + "\n")
 				self.log.close()
-				#print("h1")
-		elif float(incRTTsum) + 0.25 < float(self.rttsums[self.hubnode]) and self.hubnode != nodeName:
-			#print("stuck4")
-			self.log = open(self.logfilename, 'a')
-			self.log.write("Updated hub from " + self.hubnode + " to " + nodeName + " at " + str(time.time()) + "\n")
-			self.log.close()
-			self.hubnode = nodeName
-			#print("h2")
-		elif self.hubnode == nodeName:
-			# print("stuck5")
-			# if float(incRTTsum) > float(oldRTTsum):
-			# 	x = 1
-			#print("stuck5a")
-			if float(incRTTsum) > float(oldRTTsum):
-				# print("stuckA")
-				tempmin = float(incRTTsum)
-				tempnode = self.hubnode
-				# print("stuckB")
-				for node in self.rttsums:
-					if float(self.rttsums[node]) < float(tempmin):
-						# print("stuckC")
-						tempmin = float(self.rttsums[node])
-						tempnode = node
-				# print("stuckD")
-				if float(self.rttsum) < float(tempmin):
-					# print("stuckE")
-					tempmin = float(self.rttsum)
-					tempnode = name
-				# print("stuckF")
-				if float(tempmin) + 0.25 < float(incRTTsum):
-					# print("stuckG")
-					self.hubnode = tempnode
-					self.log = open(self.logfilename, 'a')
-					self.log.write("Updated hub from " + self.hubnode + " to " + tempnode + " at " + str(time.time()) + "\n")
-					self.log.close()
-			# print("stuckI")
-		# print("stuck6")
-		if nodeName not in self.rttsums:
-			self.rttsums[nodeName] = incRTTsum
-		# print("stuck7")
-		# print("Post-receive status")
-		# print(name + ": " + str(self.rttsum))
-		# for node in self.rttsums:
-		# 	print(node + " " + str(self.rttsums[node]))
-		# print("Hubnode: " + self.hubnode)
+				self.hubnode = nodeName
+				#print("h2")
+			elif self.hubnode == nodeName:
+				# print("stuck5")
+				# if float(incRTTsum) > float(oldRTTsum):
+				# 	x = 1
+				#print("stuck5a")
+				if float(incRTTsum) > float(oldRTTsum):
+					# print("stuckA")
+					tempmin = float(incRTTsum)
+					tempnode = self.hubnode
+					# print("stuckB")
+					for node in self.rttsums:
+						if float(self.rttsums[node]) < float(tempmin):
+							# print("stuckC")
+							tempmin = float(self.rttsums[node])
+							tempnode = node
+					# print("stuckD")
+					if float(self.rttsum) < float(tempmin):
+						# print("stuckE")
+						tempmin = float(self.rttsum)
+						tempnode = name
+					# print("stuckF")
+					if float(tempmin) + 0.25 < float(incRTTsum):
+						# print("stuckG")
+						self.hubnode = tempnode
+						self.log = open(self.logfilename, 'a')
+						self.log.write("Updated hub from " + self.hubnode + " to " + tempnode + " at " + str(time.time()) + "\n")
+						self.log.close()
+				# print("stuckI")
+			# print("stuck6")
+			if nodeName not in self.rttsums:
+				self.rttsums[nodeName] = incRTTsum
+			# print("stuck7")
+			# print("Post-receive status")
+			# print(name + ": " + str(self.rttsum))
+			# for node in self.rttsums:
+			# 	print(node + " " + str(self.rttsums[node]))
+			# print("Hubnode: " + self.hubnode)
+		ackpacket = "006" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<100}".format(packnum)
+		serverSocket.sendto(ackpacket.encode(), (self.peers[nodeName][0], int(self.peers[nodeName][1])))
 
 	def getintialrttsandhub(self, s):
 		print("Getting rtts to each node...")
 		for node in self.peers:
 			if (node != name):
 				while (node not in self.rtttimes):
-					self.startingTime = time.time()
-					self.packetTimes[int(self.packetNum)] = self.startingTime
-					self.initialSendRTT(s, node, self.packetNum)
-					self.packetNum = self.packetNum + 1
+					self.initialSendRTT(s, node)
+					#print(self.rtttimes)
 					time.sleep(1)
 
 		#print(self.rtttimes)
@@ -373,10 +448,10 @@ class Peer:
 		time.sleep(5)
 		#print("recalculating")
 		for node in self.rtttimes:
-			self.startingTime = time.time()
-			self.packetTimes[int(self.packetNum)] = self.startingTime
-			self.initialSendRTT(s, node, self.packetNum)
-			self.packetNum = self.packetNum + 1
+			#self.startingTime = time.time()
+			#self.packetTimes[int(self.packetNum)] = self.startingTime
+			self.initialSendRTT(s, node)
+			#self.packetNum = self.packetNum + 1
 		#print(self.rttsum)
 		self.rttsum = 0
 		for node in self.rtttimes:
@@ -429,32 +504,62 @@ class Peer:
 	#String message broadcasting
 
 	def sendStringMessage(self, serverSocket, stringMessage):
-		broadcastpacket = "004" + "{:<5}".format(localPort) + "{:<16}".format(name) + stringMessage
+
 		if self.hubnode == name:
+			temppacketnum = int(self.packetNum)
+			self.packetNum += 1
 			for node in self.peers:
 				if node != name:
-					serverSocket.sendto(broadcastpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+					while int(temppacketnum) not in self.receivedAcks:
+						broadcastpacket = "004" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<16}".format(name) + "{:<100}".format(temppacketnum) + stringMessage
+						serverSocket.sendto(broadcastpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+						time.sleep(1)
+				temppacketnum = int(self.packetNum)
+				self.packetNum += 1
 		else:
-			serverSocket.sendto(broadcastpacket.encode(), (self.peers[self.hubnode][0], int(self.peers[self.hubnode][1])))
+			temppacketnum = int(self.packetNum)
+			self.packetNum += 1
+			while int(temppacketnum) not in self.receivedAcks:
+						broadcastpacket = "004" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<16}".format(name) + "{:<100}".format(temppacketnum) + stringMessage
+						serverSocket.sendto(broadcastpacket.encode(), (self.peers[self.hubnode][0], int(self.peers[self.hubnode][1])))
+						time.sleep(1)
 		self.log = open(self.logfilename, 'a')
 		self.log.write("Sent message " + stringMessage + " at " + str(time.time()) + "\n")
 		self.log.close()
 
 	def receiveStringMessage(self, clientAddress, message, serverSocket):
-		print("\nReceived message from " + message[8:24])
-		print("Message: " + message[24:])
-		print("Star-node command: ", end='', flush=True)
-		if self.hubnode == name:
-			for node in self.peers:
-				if node != name and node != message[8:24].strip():
-					serverSocket.sendto(message.encode(), (self.peers[node][0], int(self.peers[node][1])))
-			self.log = open(self.logfilename, 'a')
-			self.log.write("Forwarded message " + message[24:] + " from " + message[8:24].strip() + " at " + str(time.time()) + "\n")
-			self.log.close()
-		self.log = open(self.logfilename, 'a')
-		self.log.write("Received message " + message[24:] + " from " + message[8:24].strip() + " at " + str(time.time()) + "\n")
-		self.log.close()
+		#print("Received message: " + message)
+		packnum = int(message[40:140].strip())
+		if (message[24:40].strip(), packnum) not in self.receivedPackets:
+			self.receivedPackets.add((message[24:40].strip(), packnum))
+			print("\nReceived message from " + message[8:24].strip())
+			print("Message: " + message[140:])
+			print("Star-node command: ", end='', flush=True)
+			if self.hubnode == name:
+				temppacketnum = int(self.packetNum)
+				self.packetNum += 1
+				for node in self.peers:
+					if node != name and node != message[8:24].strip():
+						while int(temppacketnum) not in self.receivedAcks:
+							forwardpacket = "004" + "{:<5}".format(localPort) + message[8:24] + "{:<16}".format(name) + "{:<100}".format(temppacketnum) + message[140:]
+							serverSocket.sendto(forwardpacket.encode(), (self.peers[node][0], int(self.peers[node][1])))
+							time.sleep(1)
+					temppacketnum = int(self.packetNum)
+					self.packetNum += 1
 
+				self.log = open(self.logfilename, 'a')
+				self.log.write("Forwarded message " + message[24:] + " from " + message[8:24].strip() + " at " + str(time.time()) + "\n")
+				self.log.close()
+			self.log = open(self.logfilename, 'a')
+			self.log.write("Received message " + message[24:] + " from " + message[8:24].strip() + " at " + str(time.time()) + "\n")
+			self.log.close()
+
+		ackpacket = "006" + "{:<5}".format(localPort) + "{:<16}".format(name) + "{:<100}".format(packnum)
+		#print("Sent ack: " + ackpacket)
+		nodeName = message[24:40].strip()
+		serverSocket.sendto(ackpacket.encode(), (self.peers[nodeName][0], int(self.peers[nodeName][1])))
+		#print("sent ack to " + nodeName)
+		#print(str((self.peers[nodeName][0])) + " " +  str(int(self.peers[nodeName][1])))
 
 	#File broadcasting
 
@@ -465,7 +570,6 @@ class Peer:
 		z = filepacketheader.encode()
 		filepacketvalue = data
 		totalfilepacket = z + data
-		print(totalfilepacket)
 		if self.hubnode == name:
 			for node in self.peers:
 				if node != name:
@@ -487,9 +591,7 @@ class Peer:
 		print(fileName)
 		tempname = name + fileName
 		f = open(tempname, 'wb')
-		#data, addr = serverSocket.recvfrom(65000)
 		data = message[40:]
-		print(message)
 		f.write(data)
 		print("Star-node command: ", end='', flush=True)
 		if self.hubnode == name:
